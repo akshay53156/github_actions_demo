f2d8a7b7fe8f319346c2d487af760ebb85aec03f
akshay53156 <akshay53156@gmail.com>
2025-08-04 23:55:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello from Node.js App!'));
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
